74fa1040cd7e81bf09b5065265a40b78b155668c
Jeremy Wright <jeremy@quiescent.us>
2021-08-05 11:54:42 -0700

    Office hours

---
+++ ProjectTest/Project3.test.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project3
+{
+    class Project1test
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestIsPrime()
+        {
+            Project_Eulers.Project3 uut = new Project_Eulers.Project3();
+            Assert.IsTrue(uut.IsPrime(5));
+        }
+        
+        [Test]
+        public void TestIsNotPrime()
+        {
+            Project_Eulers.Project3 uut = new Project_Eulers.Project3();
+            Assert.IsFalse(uut.IsPrime(20));
+        }
+        
+        [Test]
+        public void TestEulerExample()
+        {
+
+            Project_Eulers.Project3 uut = new Project_Eulers.Project3();
+            var factors = uut.factors(13195);
+            Assert.AreEqual(4, factors.Count);
+            Assert.Contains(5, factors);
+            Assert.Contains(7, factors);
+            Assert.Contains(13,factors); 
+            Assert.Contains(29,factors);
+
+        }
+    }
+}
--- project3/Project3.cs
+++ project3/Project3.cs
@@ -4,12 +4,42 @@ using System.Text;
 
 namespace Project_Eulers
 {
-    class Project3
+    public class Project3
     {
+        public bool IsPrime(int number)
+        {
+            if(number == 1) // 1 - not a prime number
+                return false;
+            for (int d = 2; d * d <= number; d++)
+            {
+                if (number % d == 0)
+            return false;
+            }
+            return true;
+        }
+
+        public List<int> factors(int number)
+        {
+            List<int> primes = new List<int>();
+            if(number % 2 == 0)
+                primes.Add(2);
+            //We know 1, and 2 aren't prime so let's start at 3.
+            //We know that even numbers aren't prime, so let's skip those.
+            for(int i = 3; i < number; i += 2)
+            {
+                if(IsPrime(i) && number % i == 0)
+                {
+                    primes.Add(i);
+                }
+
+            }
+            return primes;
+        }
+
         static void Main()
         {
             string readline;
-            long BigNumber = 600851475143;
+            long BigNumber = 15;
             long answer = 0;
             int loopbreaker = 1;
 
